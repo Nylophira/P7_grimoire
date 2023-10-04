b7887417fbd789673be13d556131718ebe0686b8
Nylophira <121953951+Nylophira@users.noreply.github.com>
2023-10-04 20:23:34 +0200

    modif route

--- backend/routes/rBook.js
+++ backend/routes/rBook.js
@@ -5,8 +5,8 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer");
 
 router.get('/', control.readAll);
-router.get('/bestrating', control.bestRating);
 router.post('/', auth, multer, control.updateAll);
+router.get('/bestrating', control.bestRating);
 router.get('/:id', control.readOne);
 router.put('/:id', auth, multer, control.updateOne);
 router.delete('/:id', auth, control.delOne);
